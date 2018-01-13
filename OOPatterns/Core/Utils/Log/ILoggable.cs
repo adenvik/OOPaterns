dba063868f36da691a0f5a493671fc3d3ddeac3a
adenvik <d3n4ik@mail.ua>
2018-01-14 00:31:08 +0300

    log

---
+++ OOPatterns/Core/Utils/Log/ILoggable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPatterns.Core.Utils.Log
+{
+    interface ILoggable
+    {
+        void SetPathToFile(string Path);
+        void Log(string Message);
+    }
+}
---
+++ OOPatterns/Core/Utils/Log/Logger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPatterns.Core.Utils.Log
+{
+    class Logger : ILoggable
+    {
+        string PathToFile = "";
+
+        public void Log(string Message)
+        {
+            using(StreamWriter sw = new StreamWriter(new FileStream(PathToFile, FileMode.Append)))
+            {
+                sw.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}\t{Message}");
+            }
+        }
+
+        public void SetPathToFile(string Path)
+        {
+            PathToFile = Path;
+        }
+    }
+}
